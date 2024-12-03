11b78563f11fc086403784ab6a80ffbc7a209b89
Habs <74028745+Habs-1@users.noreply.github.com>
2024-12-03 14:54:25 -0500

    removed legacy code main.py

--- main.py
+++ main.py
@@ -4,10 +4,6 @@ from pull_data import events_to_dataframe_allCol
 from transform import flatten_nested_json
 from transform import init_drop
 from transform import transform_data
-# from pull_data import get_existing_events
-# from pull_data import filter_new_events
-# from sqlalchemy import create_engine
-# import os
 from dotenv import load_dotenv
 import warnings
 
@@ -21,27 +17,9 @@ def run_pipeline():
     events = fetch_events()
     print("### Events Fetched ###")
 
-    # Changed mind on removing existing events so updates are reflected. Will load and replace any existing event IDs
-    #
-    # print('### Checking existing Events ###')
-    # engine = create_engine(f'snowflake://{os.getenv("SNOWFLAKE_USER")}:{os.getenv("SNOWFLAKE_PASSWORD")}@{os.getenv("SNOWFLAKE_ACCOUNT")}/{os.getenv("SNOWFLAKE_DATABASE")}/{os.getenv("SNOWFLAKE_SCHEMA")}?warehouse={os.getenv("SNOWFLAKE_WAREHOUSE")}')
-    # existing_events_df = get_existing_events(engine)
-    # engine.dispose()
-    # existing_events_id = existing_events_df['id']
-    # if not existing_events_df.empty:
-    #     print('### Existing Events Found ###')
-    # else:
-    #     print("### No Existing Events Found ###")
-
     print("### Creating DF to Load Events ###")
     df = events_to_dataframe_allCol(events)
     print("### DF Created ###")
-
-    # Changed mind on removing existing events so updates are reflected. Will load and replace any existing event IDs
-    # print("### Removing Existing Events ###")
-    # new_events_df = filter_new_events(df, existing_events_id)
-    # new_events_len = len(new_events_df)
-    # print(f"### {new_events_len} New Events to Add ###")
 
     print("### Cleaning DF of Events ###")
     df_clean1 = init_drop(df)
